879b4615c771615cd32173a1bcbb5046dda93832
filemon <jiri.fabian@gmail.com>
2021-01-29 12:27:04 +0100

    Random hotspot name in order to allow several boxes to be up

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,33 +1,39 @@
-#!/usr/bin/env bash
+**#!/usr/bin/env bash**
 
-export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
+export DBUS\_SYSTEM\_BUS\_ADDRESS=unix:path=/host/run/dbus/system\_bus\_socket
 
-# Optional step - it takes couple of seconds (or longer) to establish a WiFi connection
+_# Optional step - it takes couple of seconds (or longer) to establish a WiFi connection
 # sometimes. In this case, following checks will fail and wifi-connect
 # will be launched even if the device will be able to connect to a WiFi network.
-# If this is your case, you can wait for a while and then check for the connection.
-# sleep 15
+# If this is your case, you can wait for a while and then check for the connection._
+sleep 15
 
-# Choose a condition for running WiFi Connect according to your use case:
+_# Choose a condition for running WiFi Connect according to your use case:_
 
-# 1. Is there a default gateway?
-# ip route | grep default
+_# 1. Is there a default gateway?
+# ip route | grep default_
 
-# 2. Is there Internet connectivity?
-# nmcli -t g | grep full
+_# 2. Is there Internet connectivity?
+# nmcli -t g | grep full_
 
-# 3. Is there Internet connectivity via a google ping?
-# wget --spider http://google.com 2>&1
+_# 3. Is there Internet connectivity via a google ping?
+# wget --spider http://google.com 2>&1_
 
-# 4. Is there an active WiFi connection?
+last4\_mac=`cat /sys/class/net/eth0/address | sed **'s/://g'**`
+last4\_mac=`echo $last4\_mac | tail -c 5`
+ssid=**"**$PORTAL\_SSID**-**$last4\_mac**"****
+
+
+**_# 4. Is there an active WiFi connection?_
 iwgetid -r
 
-if [ $? -eq 0 ]; then
-    printf 'Skipping WiFi Connect\n'
-else
-    printf 'Starting WiFi Connect\n'
-    ./wifi-connect
-fi
+**if** [ $? -eq 0 ]; **then**
+    printf **'Skipping WiFi Connect\\n'
+else**
+    printf **'Starting WiFi Connect\\n'**
+    ./wifi-connect -s $ssid
+**fi**
 
-# Start your application here.
+_# Start your application here._
 sleep infinity
+
